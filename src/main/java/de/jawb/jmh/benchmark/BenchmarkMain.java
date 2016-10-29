c2414a9c71ffef6af72bc3be2c45972ac0fdfd5b
dit-j <krausdieter@gmx.net>
2016-10-29 15:25:58 +0200

    removed main method

--- src/main/java/de/jawb/jmh/benchmark/BenchmarkMain.java
+++ src/main/java/de/jawb/jmh/benchmark/BenchmarkMain.java
@@ -6,21 +6,20 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import de.jawb.jmh.benchmark.example.bool.BitSetArray;
 import de.jawb.jmh.benchmark.example.bool.IBoolArray;
 import de.jawb.jmh.benchmark.example.bool.SimpleArray;
 
-@Fork(value = 3)
+@State(Scope.Benchmark)
+@Fork(value = 1)
 @Warmup(iterations = 4, time = 200, timeUnit = TimeUnit.MILLISECONDS)
-@Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 3, time = 500, timeUnit = TimeUnit.MILLISECONDS)
 public class BenchmarkMain {
 
     @Param({ "100", "1000", "10000", "100000", "1000000", "10000000" })
@@ -54,14 +53,5 @@ public class BenchmarkMain {
     @Benchmark
     public void bitset(Blackhole bh) {
         doit(bh, bitset);
-    }
-
-    public static void main(String[] args) throws RunnerException {
-        
-        Options opt = new OptionsBuilder() //
-                .include(BenchmarkMain.class.getSimpleName())
-                .build();
-
-        new Runner(opt).run();
     }
 }
